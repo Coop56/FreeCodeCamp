869a79d389e9828d76798ef6cde85095205d1ae7
Coop56 <JCooper@jeremiahcooper.com>
2015-11-25 02:41:08 -0500

    Completed Seek and Destroy Bonfire

---
+++ Basic Algorithm Scripting/seek-and-destroy.js
@@ -0,0 +1,22 @@
+/*
+
+Bonfire: Seek and Destroy
+
+You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+
+*/
+function destroyer(arr) {
+  var args = Array.prototype.slice.call(arguments);
+  args.splice(0, 1);
+  return arr.filter(function(element) {
+    return args.indexOf(element) === -1;
+  });
+}
+
+// Test Cases
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3);
+destroyer([3, 5, 1, 2, 2], 2, 3, 5);
+destroyer([2, 3, 2, 3], 2, 3);
+destroyer(["tree", "hamburger", 53], "tree", 53);
