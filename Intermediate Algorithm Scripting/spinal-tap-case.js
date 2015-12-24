099bc51fc919cc26b454450c8c6779985f75c39a
Jeremiah Cooper <JCooper@katemiah.com>
2015-12-24 14:11:17 -0500

    Completed Spinal Tap Case Bonfire

---
+++ Intermediate Algorithm Scripting/spinal-tap-case.js
@@ -0,0 +1,21 @@
+/*
+
+Bonfire: Spinal Tap Case
+
+Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+*/
+
+function spinalCase(str) {
+ 
+  return str.replace(/([a-z])([A-Z])/g, '$1 $2').replace(/\s+|_+/g, "-").toLowerCase();
+  
+  //return str.split(' ').join('-').toLowerCase();
+}
+
+// Test Cases
+
+spinalCase('This Is Spinal Tap');
+spinalCase("thisIsSpinalTap");
+spinalCase("The_Andy_Griffith_Show");
+spinalCase("Teletubbies say Eh-oh");
